a195f6aca9ff73a619b47d18c459c2246ae89b84
Rohat <ryildirim2@roc-dev.com>
2021-03-29 12:01:14 +0200

    H5 Dynamic Website

---
+++ H5/dier.php
@@ -0,0 +1,8 @@
+<?php
+if ($_POST['dier'] === "beer") {
+    echo "<img src='IMG/beer.png'>";
+} elseif ($_POST['dier'] === "Tijger") {
+    echo "<img src='IMG/tijger.jpg'>";
+} elseif ($_POST['dier'] === "otter") {
+    echo "<img src='IMG/otter.jpg'>";
+}
---
+++ H5/diercheck.php
@@ -0,0 +1,8 @@
+<?php
+if (isset($_POST['beer'])) {
+    echo "<img src='IMG/beer.png'>";
+} if (isset($_POST['tijger'])) {
+    echo "<img src='IMG/tijger.jpg'>";
+} if (isset($_POST['otter'])) {
+    echo "<img src='IMG/otter.jpg'>";
+}
